efaa7080123a5bbec713fb4a3b8ad668c5ae517d
luisa <luisafernandamajin0222@gmail.com>
2024-08-06 17:55:15 -0500

    Final

---
+++ reto-arrays-main/src/Main.java
@@ -0,0 +1,87 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1. Declaración e inicialización de un arreglo
+        int[] numeros = {5, 2, 8, 1, 9, 3, 7, 4, 6};
+
+        // 2. Imprimir el arreglo original
+        System.out.println("Arreglo original:");
+        imprimirArreglo(numeros);
+
+        // 3. Encontrar el número mayor
+        int mayor = encontrarMayor(numeros);
+        System.out.println("El número mayor es: " + mayor);
+
+        // 4. Calcular el promedio
+        double promedio = calcularPromedio(numeros);
+        System.out.println("El promedio es: " + promedio);
+
+        // 5. Invertir el arreglo
+        invertirArreglo(numeros);
+        System.out.println("Arreglo invertido:");
+        imprimirArreglo(numeros);
+
+        // 6. Ordenar el arreglo (implementa el método ordenarArreglo)
+        ordenarArreglo(numeros);
+        System.out.println("Arreglo ordenado:");
+        imprimirArreglo(numeros);
+    }
+
+    // Método para imprimir el arreglo
+    public static void imprimirArreglo(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Método para encontrar el número mayor
+    public static int encontrarMayor(int[] arr) {
+        int mayor = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > mayor) {
+                mayor = arr[i];
+            }
+        }
+        return mayor;
+    }
+
+    // Método para calcular el promedio
+    public static double calcularPromedio(int[] arr) {
+        int suma = 0;
+        for (int num : arr) {
+            suma += num;
+        }
+        return (double) suma / arr.length;
+    }
+
+    // Método para invertir el arreglo
+    public static void invertirArreglo(int[] arr) {
+        int inicio = 0;
+        int fin = arr.length - 1;
+        while (inicio < fin) {
+            int temp = arr[inicio];
+            arr[inicio] = arr[fin];
+            arr[fin] = temp;
+            inicio++;
+            fin--;
+        }
+    }
+
+    // Método para ordenar el arreglo (algoritmo de burbuja)
+    public static void ordenarArreglo(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Intercambiar arr[j] y arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+
